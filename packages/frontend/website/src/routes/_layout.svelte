d1c7bf319c92e4c235657d557dfabc4905f09d19
André Markwalder <andre.markwalder@gmail.com>
2020-06-17 16:36:28 +0200

    Initial commit

---
+++ packages/frontend/website/src/routes/_layout.svelte
@@ -0,0 +1,49 @@
+<style lang="scss" global>
+    @import '../styles/global.scss';
+</style>
+
+<script>
+    import { onMount } from 'svelte';
+
+    import Header from '../components/Header.svelte';
+    import Footer from '../components/Footer.svelte';
+
+    export let segment;
+
+    let materializeCssReady = false;
+    let mounted = false;
+
+    onMount(() => {
+        mounted = true;
+        if (materializeCssReady) {
+            initiMaterializeCss();
+        }
+    });
+
+    function materializeCssLoaded() {
+        materializeCssReady = true;
+        if (mounted) {
+            initiMaterializeCss();
+        }
+    }
+
+    function initiMaterializeCss() {
+        M.AutoInit();
+    }
+</script>
+
+<svelte:head>
+    <title>Badminton Club Effretikon</title>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+        on:load="{materializeCssLoaded}"
+    ></script>
+</svelte:head>
+
+<Header></Header>
+
+<main>
+    <slot segment="{segment}"></slot>
+</main>
+
+<Footer></Footer>
