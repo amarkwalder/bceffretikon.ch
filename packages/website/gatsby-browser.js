0128bbbb0dce70c1d89672876d1045e05793f24c
André Markwalder <andre.markwalder@gmail.com>
2020-10-02 23:51:49 +0200

    config error

--- packages/website/gatsby-browser.js
+++ packages/website/gatsby-browser.js
@@ -2,6 +2,7 @@ import { AuthorsField } from './src/fields/Authors'
 
 const translations = require('./content/settings/translations.json')
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const onClientEntry = () => {
     window.tinacms.fields.add({
         name: 'authors',
