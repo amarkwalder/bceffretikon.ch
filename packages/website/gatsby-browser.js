33ab5964d0ae1444a64473d0c307ddc53ea10603
André Markwalder <andre.markwalder@gmail.com>
2020-10-03 04:03:52 +0200

    TinaCMS status check

--- packages/website/gatsby-browser.js
+++ packages/website/gatsby-browser.js
@@ -4,6 +4,8 @@ const translations = require('./content/settings/translations.json')
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const onClientEntry = () => {
+    console.log('tinacms.enabled', window.tinacms.enabled)
+    console.log('tinacms.disabled', window.tinacms.disabled)
     window.tinacms.fields.add({
         name: 'authors',
         Component: AuthorsField,
